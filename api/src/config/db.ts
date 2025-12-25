76889f4063f163a268cfc4bb12d5ca0e0b902df2
Gerti Rexhepi <gertirexhepi16@example.com>
2025-12-25 15:03:35 +0100

    Remove old api folder (prepare to replace)

--- api/src/config/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  database: "oxa_cafe",
-  user: "oxa_cafe_user",
-  password: process.env.DB_PASSWORD,
-});
